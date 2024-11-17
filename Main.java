e501d1c2ea97db3c05362c60d63bc7dba01f6648
akash <ashpatil4040@gmail.com>
2024-11-16 20:09:55 -0600

    modified

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
   System.out.println("this is a java program");
   System.out.println("Output from java program");
   System.out.println(new Date());
+  System.out.println("hello");
   
   
 }
